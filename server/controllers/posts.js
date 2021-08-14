81ad2273d35befe601edf83b14ea72c55197c618
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-08-14 23:13:44 +0100

    the two controller added , get post and create post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,9 +9,22 @@ import PostMessage from '../models/postMessage.js';
 export const getPosts = async (req, res) => { 
     try {
         const postMessages = await PostMessage.find();
+        // console.log(postMessages)
                 
         res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
+}
+export const createPost = async (req, res) => {
+
+    const post = req.body;
+    const newpost = new  PostMessage(post);
+    try {
+        await newpost.save();
+        res.status(201).json(newpost);
+    } catch (error) {
+        res.status(409).json({message:error.message})
+    }
+
 }
