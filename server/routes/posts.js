1a8ac8b7febcd6fd1e90242770c897059f10b6f7
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-08-15 01:03:21 +0100

    update controller method added for updating the post -patch request on the route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/', getPosts);
 router.post('/', createPost);
 router.get('/:id', getPost);
+router.patch('/:id', updatePost);
 
 
 export default router;
