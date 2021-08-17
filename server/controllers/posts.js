237ec52b47d43cf32bf961f9e6c6de99de5d6477
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-08-17 14:32:48 +0100

    server post controller added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-import express from 'express';
+// import express from 'express';
 import mongoose from 'mongoose';
 
 import PostMessage from '../models/postMessage.js';
@@ -59,4 +59,15 @@ export const deletePost = async (req, res) => {
     await PostMessage.findByIdAndRemove(id);
 
     res.json({ message: "Post deleted successfully." });
+}
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
 }
