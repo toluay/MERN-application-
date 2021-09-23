d890aa6f138542b3c9952b02be98b7bad4b8e3d3
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-23 10:48:42 +0100

    the post instaed of posts route debugged and corrected

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 dotenv.config();
 
-// app.use(bodyParser.json({ limit: '30mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 const CONNECTION_URL ='mongodb://localhost:27017';
 
