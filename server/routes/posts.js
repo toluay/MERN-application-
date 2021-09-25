91ae538be61a0c96ea70c248d824347fe6936683
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-25 12:09:29 +0100

    getPostsBySearch added to the backend at the router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getPosts, getPost, createPost,updatePost,deletePost,likePost } from '../controllers/posts.js';
+import { getPosts, getPost, createPost,updatePost,deletePost,likePost ,getPostsBySearch} from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
 
 router.get('/', getPosts);
+router.get('/search', getPostsBySearch);
 router.post('/',auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
