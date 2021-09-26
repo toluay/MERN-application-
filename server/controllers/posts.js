46aa0cd0f8f8917e0b6d8736138bc6d619823214
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-26 22:28:24 +0100

    post server controller adjiusted to get the page as an inpit from the frontend the value is used for getting post data for pagoination

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,13 +6,18 @@ import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
-export const getPosts = async (req, res) => { 
+export const getPosts = async (req, res) => {
+    const { page } = req.query;
+    
     try {
-        const postMessages = await PostMessage.find();
-     console.log(postMessages)
-                
-        res.status(200).json(postMessages);
-    } catch (error) {
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    
+        const total = await PostMessage.countDocuments({});
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+
+        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
+    } catch (error) {    
         res.status(404).json({ message: error.message });
     }
 }
