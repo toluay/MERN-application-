e8d063e44e0cdb2ce82b92a615e0d50129f29b13
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-27 17:23:31 +0100

    commentPost added to the function in the controller , a function use d by the route of the API

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, getPost, createPost,updatePost,deletePost,likePost ,getPostsBySearch} from '../controllers/posts.js';
+import { getPosts, getPost, createPost,updatePost,deletePost,likePost ,getPostsBySearch,commentPost} from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
@@ -12,5 +12,6 @@ router.post('/',auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost',auth,  commentPost);
 
 export default router;
