27d2fbd476d0445c8d02776a7c7ea89ea7967b32
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-27 16:43:36 +0100

    getpost route added to the backend

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,6 +7,7 @@ import auth from "../middleware/auth.js";
 
 router.get('/', getPosts);
 router.get('/search', getPostsBySearch);
+router.get('/:id', getPost);
 router.post('/',auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
