7b28a203ef48994d833e86b3704fc50a46ae598c
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-10-04 15:13:36 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -13,11 +13,14 @@ dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+app.get('/',(req, res)=>{
+  res.send('APP IS RUNNING');
+});
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL ='mongodb://localhost:27017';
+const CONNECTION_URL ='mongodb+srv://newuser:System01@cluster0.4henh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT|| 5000;
 
